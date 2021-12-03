84fa8edd2904f2c6bad3bcd6050ecf9f954f876c
Benedict Schurwanz <19333755+benedictify@users.noreply.github.com>
2021-12-02 19:24:51 -0600

    init commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+
+const server = express()
+
+server.get('/api/users', (req, res) => {
+	res.json([
+		{ id: 1, username: "foo" },
+		{ id: 2, username: "bar" },
+		{ id: 3, username: "baz" },
+	])
+})
+
+const PORT = process.env.PORT || 8080
+// on Heroku, this will be their port
+
+server.listen(PORT, () => {
+	console.log(`listening on ${PORT}`)
+})
+
